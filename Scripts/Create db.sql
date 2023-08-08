fb0492c9794eaa10d863e6dbaf83201f38b73ca9
MVMmaksM <vaganov.maksim94@yandex.ru>
2023-08-08 08:47:43 +0500

    Create db

---
+++ Scripts/Create db.sql
@@ -0,0 +1,72 @@
+USE [master]
+
+DECLARE @kill VARCHAR(8000) = '';  
+SELECT @kill = @kill + 'kill ' + CONVERT(VARCHAR(5), session_id) + ';'  
+FROM sys.dm_exec_sessions
+WHERE database_id  = db_id('CuttingMapsDetail')
+
+EXEC(@kill);
+
+GO
+
+IF DB_ID('CuttingMapsDetail') IS NOT NULL
+	DROP DATABASE CuttingMapsDetail
+GO
+CREATE DATABASE CuttingMapsDetail
+GO
+USE CuttingMapsDetail
+GO
+CREATE TABLE [dbo].[Material]
+(
+	[Id] BIGINT IDENTITY(1,1) NOT NULL,
+	[Title] NVARCHAR (100) NOT NULL,
+	[FullName] NVARCHAR (200) NOT NULL,
+	[Thick] REAL NOT NULL,
+
+	CONSTRAINT PK__Material PRIMARY KEY CLUSTERED([Id])
+)
+GO
+CREATE TABLE [dbo].[Sheet] 
+(
+	[Id] BIGINT IDENTITY(1,1) NOT NULL,
+	[Title] NVARCHAR (100) NOT NULL,
+	[FullName] NVARCHAR (200) NOT NULL,
+	[Width] REAL NOT NULL,
+	[Height] REAL NOT NULL,
+
+	CONSTRAINT PK__Sheet PRIMARY KEY CLUSTERED([Id])
+)
+GO
+CREATE TABLE [dbo].[CuttingMap]
+(
+	[Id] BIGINT IDENTITY(1,1) NOT NULL,
+	[Title] NVARCHAR (100) NOT NULL,
+	[FullName] NVARCHAR (200) NOT NULL,
+	[MaterialId] BIGINT NOT NULL,
+	[SheetId] BIGINT NOT NULL,
+
+	CONSTRAINT PK__CuttingMap PRIMARY KEY CLUSTERED([Id]),
+	CONSTRAINT FK__CuttingMap_MaterialId FOREIGN KEY([MaterialId]) REFERENCES [dbo].[Material]([Id]),
+	CONSTRAINT FK__CuttingMap_SheetId FOREIGN KEY([SheetId]) REFERENCES [dbo].[Sheet]([Id])
+)
+GO
+CREATE TABLE [dbo].[Detail]
+(
+	[Id] BIGINT IDENTITY(1,1) NOT NULL,
+	[Title] NVARCHAR (100) NOT NULL,
+	[FullName] NVARCHAR (200) NOT NULL,
+	[Countours] VARBINARY(MAX) NOT NULL,
+
+	CONSTRAINT PK__Detail PRIMARY KEY CLUSTERED([Id])
+)
+GO
+CREATE TABLE [dbo].[CuttingMapDetail]
+(
+	[CuttingMapId] BIGINT NOT NULL,
+	[DetailId] BIGINT NOT NULL,
+
+	CONSTRAINT PK__CuttingMapDetail PRIMARY KEY CLUSTERED([CuttingMapId],[DetailId]),
+	CONSTRAINT FK__CuttingMapDetail_CuttingMapId FOREIGN KEY([CuttingMapId]) REFERENCES [dbo].[CuttingMap]([Id]),
+	CONSTRAINT FK__CuttingMapDetail_DetailId FOREIGN KEY([DetailId]) REFERENCES [dbo].[Detail]([Id])
+
+)
